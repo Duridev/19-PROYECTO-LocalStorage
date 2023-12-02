25155db6e286091958ea032af3763f7cd88955a4
duridev <duridev@outlook.com>
2023-12-01 23:15:54 -0300

    validando el formulario

--- js/app.js
+++ js/app.js
@@ -17,8 +17,30 @@ function eventListeners() {
 // ********  FUNCIONES       **********
 function agregarTweet(e) {
     e.preventDefault();
+
+    const tweet = document.querySelector('#tweet').value;
+
+    if(tweet === '') {
+        mostrarError('Un mensaje no puede ir vacío');
+        return; // previene que se sigan ejecutando más líneas de codigo
+    }
+    console.log('Agregando tweet');
 }
 
+//-------
 
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error')
+
+    //Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 2300);
+}
 
 
